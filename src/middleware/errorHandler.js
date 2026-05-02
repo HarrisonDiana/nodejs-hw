f28d6c01e4d996d14fee1075536d8f699dda73b6
Harrison Diana <dharrison77777@gmail.com>
2026-05-02 15:48:07 +0200

    add index file

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,20 +1,6 @@
-// src/middleware/errorHandler.js
-
-import { HttpError } from 'http-errors';
-
-export const errorHandler = (error, req, res, next) => {
-  // Якщо помилка створена через http-errors
-  if (error instanceof HttpError) {
-    return res.status(error.status).json({
-      message: error.message || error.name,
-    });
-  }
-
-  const isProd = process.env.NODE_ENV === 'production';
-
-  // Усі інші помилки — як внутрішні
+export const errorHandler = (error, req, res, _next) => {
   console.error(error);
   res.status(500).json({
-    message: isProd ? 'Internal server error' : error.message,
+    message: error.message,
   });
 };
