84c7da54e5ff12a1b69f9ec466ebf4e9e8ad55eb
Harrison Diana <dharrison77777@gmail.com>
2026-05-14 14:23:43 +0200

    add index file

--- src/middleware/errorHandler.js
+++
@@ -1,6 +0,0 @@
-export const errorHandler = (error, req, res, _next) => {
-  console.error(error);
-  res.status(500).json({
-    message: error.message,
-  });
-};
--- src/middleware/notFoundHandler.js
+++
@@ -1,5 +0,0 @@
-// src/middleware/notFoundHandler.js
-
-export const notFoundHandler = (req, res) => {
-  res.status(404).json({ message: 'Route not found' });
-};
