9d504495d4858807037196a5b022c0767c129c54
Harrison Diana <dharrison77777@gmail.com>
2026-05-25 17:34:20 +0200

    add index file

--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ import { errorHandler } from './middleware/errorHandler.js';
 
 import authRoutes from './routes/authRoutes.js';
 import notesRoutes from './routes/notesRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
@@ -26,6 +27,7 @@ app.use(
 app.use(cookieParser());
 
 app.use(authRoutes);
+app.use(userRoutes);
 app.use(notesRoutes);
 
 app.use(errors());
